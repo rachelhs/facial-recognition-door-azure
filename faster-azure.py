9803707f9a3c3735325c1fae51fc7c44a8a6d26d
rachelhs <rachelsmith400@hotmail.co.uk>
2020-09-18 14:28:25 +0100

    green background

--- faster-azure.py
+++ faster-azure.py
@@ -95,8 +95,8 @@ def random_persona():
 
 #display target age and target gender
 def display_target_cat():
-	target_age_text = GenerateText((60, 35), fontsize, 'white', 'black', f"{age}")
-	background[51: (51+35) ,720: (720+60)] = target_age_text
+	target_age_text = GenerateText((35, 20), fontsize, 'white', 'black', f"{age}")
+	background[46: (46+20) ,717: (717+35)] = target_age_text
 
 #display age and gender from last photo categorisation
 def display_last_cat():

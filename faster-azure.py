eb099222eeed8c40bd12f8ee9c87ba7883ccb272
rachelhs <rachelsmith400@hotmail.co.uk>
2020-09-18 13:14:43 +0100

    green background

--- faster-azure.py
+++ faster-azure.py
@@ -131,13 +131,15 @@ def access_granted_display(latest_image_string):
 
 	last_img = cv2.resize(latest_image, (last_photo_width, last_photo_height))
 	layered_img = cv2.addWeighted(last_img, 1, green_mask, 1, 0)
-	layered_img_2 = cv2.addWeighted(layered_img,1,access_granted_image_alpha,1,1)
+	#layered_img_2 = cv2.addWeighted(layered_img,1,access_granted_image_alpha,1,1)
+	layered_img_2 = cv2.add(layered_img,access_granted_image_alpha)
+
 
 	if (enter == True):
 		if (gender == 'female'):
 			green_background = cv2.imread('female-green-stretched.png')
 			green_background[120:(120+last_photo_height), 25: (25+last_photo_width)] = layered_img_2
-			green_background[51: (51+20) ,720: (720+35)] = target_age_text
+			green_background[46: (46+20) ,715: (715+35)] = target_age_text
 
 		elif (gender == 'genderless'):
 			green_background = cv2.imread('genderless-green-stretched.png')

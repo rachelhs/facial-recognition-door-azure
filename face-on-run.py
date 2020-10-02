4ef95922af2064ab7d40cbb06a4d317f7edf0371
rachelhs <rachelsmith400@hotmail.co.uk>
2020-10-02 13:06:54 +0100

    longer display age text

--- face-on-run.py
+++ face-on-run.py
@@ -128,7 +128,7 @@ def latest_file():
 #display target age and target gender
 def display_target_cat():
 	target_age_text = GenerateText((55, 20), fontsize, 'white', 'black', f"{age}")
-	background[46: (46+20) ,717: (717+35)] = target_age_text
+	background[46: (46+20) ,717: (717+55)] = target_age_text
 
 #display age and gender from last photo categorisation
 def display_last_cat():
